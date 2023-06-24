22309e76e2660ec354f59f02dc3f095fb932223c
ipowerful <eley81@gmail.com>
2023-06-24 12:10:59 +0300

    modify color migration

--- database/migrations/2023_06_22_090427_create_colors_table.php
+++ database/migrations/2023_06_22_090427_create_colors_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('colors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('alias');
             $table->timestamps();
         });
     }
