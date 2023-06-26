dfba3e2cd2a1969cddf2081f321afed645827a97
ipowerful <eley81@gmail.com>
2023-06-26 20:28:29 +0300

    update routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ColorController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('colors', ColorController::class);
+
+//Route::apiResource('colors', 'App\Http\Controllers\ColorController');
+//Route::resource('colors', 'App\Http\Controllers\ColorController');
+//Route::get('colors', 'ColorController@index');
