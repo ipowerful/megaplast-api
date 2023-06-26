70742ad990d1a2f1556563008e47c73d0d4f95db
ipowerful <eley81@gmail.com>
2023-06-26 21:13:38 +0300

    feat: add color controller methods

--- app/Http/Controllers/ColorController.php
+++ app/Http/Controllers/ColorController.php
@@ -11,4 +11,14 @@ class ColorController extends Controller
     {
         return Color::all();
     }
+
+    public function show($id)
+    {
+        return Color::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        return Color::create($request->all());
+    }
 }
