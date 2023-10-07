08c0e47df5d2a22135aa99b7595ac4f5759dfd36
ipowerful <eley81@gmail.com>
2023-10-07 07:47:54 +0300

    add migration

---
+++ database/migrations/2023_10_04_154742_create_product_colors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Product;
+use App\Models\Color;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_colors', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('price', 9, 0);
+            $table->decimal('price_old', 9, 0);
+            $table->boolean('is_in_stock')->default(false);
+            $table->foreignIdFor(Product::class)->unsigned();
+            $table->foreignIdFor(Color::class)->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_colors');
+    }
+};
