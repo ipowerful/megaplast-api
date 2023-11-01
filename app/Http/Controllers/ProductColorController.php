393d6ff9564a1fccc6e1aa28e69e3e6fb710c51a
ipowerful <eley81@gmail.com>
2023-11-01 11:08:48 +0300

    add ProductColor resource

--- app/Http/Controllers/ProductColorController.php
+++ app/Http/Controllers/ProductColorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\BaseController as BaseController;
 use App\Http\Requests\ProductColorsRequest;
+use App\Http\Resources\ProductColorResource;
 use App\Models\ProductColor;
 
 class ProductColorController extends BaseController
@@ -20,7 +21,7 @@ class ProductColorController extends BaseController
      */
     public function index()
     {
-        $productColor = ProductColor::all()->sortByDesc('price')->values();
+        $productColor = ProductColorResource::collection(ProductColor::all()->sortByDesc('price')->values());
         return $this->sendResponse($productColor, 'Product colors retrieved successfully');
     }
 
